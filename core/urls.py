5f5735c4c9f7a5333f4d24d81f69c51436beff63
varun05126 <malthumkarvarun@gmail.com>
2026-01-23 00:06:38 +0530

    updated urls

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # -------- Auth --------
     path("", views.login_view, name="login"),
     path("signup/", views.signup_view, name="signup"),
     path("logout/", views.logout_view, name="logout"),
@@ -17,13 +18,16 @@ urlpatterns = [
     path("notes/add/", views.add_note, name="add_note"),
     path("notes/my/", views.my_notes, name="my_notes"),
     path("notes/library/", views.public_library, name="public_library"),
-    path("notes/save/<int:note_id>/", views.toggle_save_note, name="toggle_save_note"),
 
     # -------- Learning Goals --------
     path("goals/", views.learning_goals, name="learning_goals"),
 
-    path("study/start/", views.start_study, name="start_study"),
-    path("study/stop/", views.stop_study, name="stop_study"),
+    # -------- Study Sessions --------
     path("study/add/", views.add_study_session, name="add_study_session"),
     path("study/history/", views.study_history, name="study_history"),
+
+    # ------- GitHub Integration --------
+    path("platforms/github/connect/", views.github_connect, name="github_connect"),
+    path("platforms/github/callback/", views.github_callback, name="github_callback"),
+    path("sync/github/", views.sync_github, name="sync_github"),
 ]
